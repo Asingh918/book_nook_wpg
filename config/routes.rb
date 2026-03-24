61661b3a05844b968356c0e87ed4b6e1fadf8798
Asingh918 <asingh198@rrc.ca>
2026-03-24 18:55:29 -0500

    Fix Devise routes and controllers for user signup and login

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,17 @@
 Rails.application.routes.draw do
-  devise_for :users
   devise_for :admin_users, ActiveAdmin::Devise.config
+  devise_for :users, controllers: {
+    registrations: 'users/registrations',
+    sessions: 'users/sessions'
+  }
   ActiveAdmin.routes(self)
 
   root 'products#index'
   resources :products, only: [:index, :show]
   resources :categories, only: [:show]
 
-  get  '/cart',              to: 'cart#show',   as: 'cart'
-  post '/cart/add',          to: 'cart#add',    as: 'cart_add'
-  delete '/cart/remove',     to: 'cart#remove', as: 'cart_remove'
-  patch '/cart/update',      to: 'cart#update', as: 'cart_update'
+  get    '/cart',        to: 'cart#show',   as: 'cart'
+  post   '/cart/add',    to: 'cart#add',    as: 'cart_add'
+  delete '/cart/remove', to: 'cart#remove', as: 'cart_remove'
+  patch  '/cart/update', to: 'cart#update', as: 'cart_update'
 end
