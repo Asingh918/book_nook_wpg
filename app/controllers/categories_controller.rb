610adf04da36d79e1410c4a398ee0919238c4495
Asingh918 <asingh198@rrc.ca>
2026-03-24 18:48:39 -0500

    Add customer store - homepage, product pages and categories

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -2,5 +2,6 @@ class CategoriesController < ApplicationController
   def show
     @category = Category.find(params[:id])
     @products = @category.products.where(active: true)
+    @categories = Category.all
   end
 end
