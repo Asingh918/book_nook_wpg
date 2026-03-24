0cb9681ad63a39b27ed8c5ad80eed4ecc954471f
Asingh918 <asingh198@rrc.ca>
2026-03-24 18:52:00 -0500

    Add province reference to users table

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,26 @@
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  belongs_to :province, optional: true
+
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :email, presence: true
+
+  def full_name
+    "#{first_name} #{last_name}"
+  end
+
+  def admin?
+    role == 'admin'
+  end
+
+  def self.ransackable_attributes(auth_object = nil)
+    ["id", "email", "first_name", "last_name", "role", "created_at", "updated_at"]
+  end
+
+  def self.ransackable_associations(auth_object = nil)
+    ["province"]
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2026_03_24_235139) do
+ActiveRecord::Schema[7.2].define(version: 2026_03_24_235158) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -89,9 +89,12 @@ ActiveRecord::Schema[7.2].define(version: 2026_03_24_235139) do
     t.string "role"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "province_id", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["province_id"], name: "index_users_on_province_id"
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
   add_foreign_key "products", "categories"
+  add_foreign_key "users", "provinces"
 end
