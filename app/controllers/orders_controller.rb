6967da53a10129b2e3aa66930d5c82e72446eb5a
Asingh918 <asingh198@rrc.ca>
2026-03-24 19:32:02 -0500

    Add order history page for customers

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,6 +1,10 @@
 class OrdersController < ApplicationController
   before_action :authenticate_user!
 
+  def index
+    @orders = Order.where(user: current_user).includes(:order_items, :province).order(created_at: :desc)
+  end
+
   def confirmation
     @order = Order.find(params[:id])
   end
--- config/routes.rb
+++ config/routes.rb
@@ -7,16 +7,17 @@ Rails.application.routes.draw do
   ActiveAdmin.routes(self)
 
   root 'products#index'
-  resources :products,  only: [:index, :show]
+  resources :products,   only: [:index, :show]
   resources :categories, only: [:show]
+  resources :orders,     only: [:index]
 
   get    '/cart',              to: 'cart#show',              as: 'cart'
   post   '/cart/add',          to: 'cart#add',               as: 'cart_add'
   delete '/cart/remove',       to: 'cart#remove',            as: 'cart_remove'
   patch  '/cart/update',       to: 'cart#update',            as: 'cart_update'
 
-  get    '/checkout',          to: 'checkout#new',           as: 'checkout'
-  post   '/checkout/province', to: 'checkout#update_province', as: 'checkout_province'
-  post   '/checkout',          to: 'checkout#create',        as: 'checkout_create'
-  get    '/orders/:id/confirmation', to: 'orders#confirmation', as: 'order_confirmation'
+  get  '/checkout',            to: 'checkout#new',           as: 'checkout'
+  post '/checkout/province',   to: 'checkout#update_province', as: 'checkout_province'
+  post '/checkout',            to: 'checkout#create',        as: 'checkout_create'
+  get  '/orders/:id/confirmation', to: 'orders#confirmation', as: 'order_confirmation'
 end
