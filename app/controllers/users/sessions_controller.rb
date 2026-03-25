ef6e0f1e2d461cf5e0414ff4b9ca69843a199d9f
Asingh918 <asingh198@rrc.ca>
2026-03-24 19:03:27 -0500

    Fix Devise session and redirect issues

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -4,4 +4,8 @@ class Users::SessionsController < Devise::SessionsController
   def after_sign_in_path_for(resource)
     root_path
   end
+
+  def after_sign_out_path_for(resource)
+    root_path
+  end
 end
