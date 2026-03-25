ee2a3e37a58ea2fa6372337902c8976b1bd4d229
Asingh918 <asingh198@rrc.ca>
2026-03-24 19:27:30 -0500

    Add keyword search with category filter

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,7 +1,22 @@
 class ProductsController < ApplicationController
   def index
-    @products = Product.where(active: true).includes(:category)
     @categories = Category.all
+    @products = Product.where(active: true).includes(:category)
+
+    if params[:search].present?
+      search_term = "%#{params[:search].downcase}%"
+      @products = @products.where(
+        "LOWER(title) LIKE ? OR LOWER(description) LIKE ?",
+        search_term, search_term
+      )
+    end
+
+    if params[:category_id].present? && params[:category_id] != "0"
+      @products = @products.where(category_id: params[:category_id])
+    end
+
+    @selected_category = params[:category_id].to_i
+    @search_term = params[:search]
   end
 
   def show
