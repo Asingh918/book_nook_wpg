13c39566e139fbf1810597a293c839de7d939a6c
Asingh918 <asingh198@rrc.ca>
2026-03-30 16:47:49 -0500

    Add order status and categories to admin

--- app/admin/categories.rb
+++ app/admin/categories.rb
@@ -1,18 +1,41 @@
 ActiveAdmin.register Category do
+  permit_params :name, :description
 
-  # See permitted parameters documentation:
-  # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
-  #
-  # Uncomment all parameters which should be permitted for assignment
-  #
-  # permit_params :name, :description
-  #
-  # or
-  #
-  # permit_params do
-  #   permitted = [:name, :description]
-  #   permitted << :other if params[:action] == 'create' && current_user.admin?
-  #   permitted
-  # end
-  
+  filter :name
+
+  index do
+    selectable_column
+    id_column
+    column :name
+    column :description
+    column :products_count do |category|
+      category.products.count
+    end
+    actions
+  end
+
+  form do |f|
+    f.inputs do
+      f.input :name
+      f.input :description
+    end
+    f.actions
+  end
+
+  show do
+    attributes_table do
+      row :name
+      row :description
+      row :created_at
+    end
+
+    panel "Products in this Category" do
+      table_for category.products do
+        column :title
+        column :author
+        column :price_cents
+        column :active
+      end
+    end
+  end
 end
